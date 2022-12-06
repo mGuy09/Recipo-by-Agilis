7a5ba4e2489f258fbc13103d4b9ba3bde0fb5e5e
mGuy09 <tudor.andreic19@gmail.com>
2022-12-06 14:07:34 +0200

    deleted the whole project for the 4th time i think

--- Recipo-byAgilis/Recipo-byAgilis/Models/Ingredient.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Recipo_byAgilis.Models;
-
-public class Ingredient
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
--- Recipo-byAgilis/Recipo-byAgilis/Models/Recipe.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Recipo_byAgilis.Models;
-
-public class Recipe
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Steps { get; set; }
-}
