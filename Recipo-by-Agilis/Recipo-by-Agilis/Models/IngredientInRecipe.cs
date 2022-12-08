1f0e7a86b3f36ff8044c1be0efee89b187dc2343
mGuy09 <tudor.andreic19@gmail.com>
2022-12-08 12:45:48 +0200

    made migrations for identity and subscriptions, subscription controller added and model

--- Recipo-by-Agilis/Recipo-by-Agilis/Models/IngredientInRecipe.cs
+++ Recipo-by-Agilis/Recipo-by-Agilis/Models/IngredientInRecipe.cs
@@ -5,6 +5,4 @@ public class IngredientInRecipe
     public int Id { get; set; }
     public int IngredientId { get; set; }
     public int RecipeId { get; set; }
-    public Ingredient Ingredient { get; set; }
-    public Recipe Recipe { get; set; }
 }
---
+++ Recipo-by-Agilis/Recipo-by-Agilis/Models/Subscription.cs
@@ -0,0 +1,7 @@
+﻿namespace Recipo_by_Agilis.Models;
+
+public class Subscription: BaseEntity
+{
+    public string Description { get; set; }
+    public bool IsPremium { get; set; }
+}
