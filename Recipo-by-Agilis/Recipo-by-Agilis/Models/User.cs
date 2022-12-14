bb0ad73fb71bc0e3dcfe6ad2e1b3b90fae2aa98f
mGuy09 <tudor.andreic19@gmail.com>
2022-12-14 11:01:11 +0200

    added another folder of a react app

---
+++ Recipo-by-Agilis/Recipo-by-Agilis/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recipo_by_Agilis.Models;
+
+public class User
+{
+    [Required(ErrorMessage = "Please enter a username")]
+    public string UserName { get; set; }
+    [Required(ErrorMessage = "Please enter an email")]
+    public string Email { get; set; }
+    [Required(ErrorMessage = "Please enter a password")]
+    public string Password { get; set; }
+    
+}
