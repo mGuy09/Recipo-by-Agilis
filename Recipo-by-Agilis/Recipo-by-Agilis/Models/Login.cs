b0cd6ae23886d6aab0c154eb2718ede8212441d0
Madalina Dumitrascu <89780920+MadalinaDumitrascu@users.noreply.github.com>
2023-01-09 20:32:59 +0200

    created the backend for the Login feature

---
+++ Recipo-by-Agilis/Recipo-by-Agilis/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recipo_by_Agilis.Models;
+
+public class Login
+{ 
+    [Required]
+    [StringLength(50)]
+    [EmailAddress]
+      public string Email { get; set; }
+
+    [Required]
+    [StringLength(50, MinimumLength = 5)]
+      public string Password { get; set; }
+}
