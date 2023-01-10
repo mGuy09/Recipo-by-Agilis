7fcf5197e80bc88275d3fb266fe8eb73dff7e071
mGuy09 <tudor.andreic19@gmail.com>
2023-01-10 09:25:06 +0200

    categoryCard.jsx updated

--- React-app-Recipo/react-app/src/Components/CategoryCard.jsx
+++ React-app-Recipo/react-app/src/Components/CategoryCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 
@@ -6,7 +6,7 @@ const CategoryCard = ({text, image, id}) => {
 
   return (
 
-    <div className='flex justify-center items-center my-10 rounded-xl text-white  border-solid border-2 w-[24rem] h-[16.5rem] duration-300 ease-in-out cursor-pointer hover:scale-110'>
+    <div className='flex justify-center items-center my-10 rounded-xl text-white w-[24rem] h-[16.5rem] duration-300 ease-in-out cursor-pointer hover:scale-110'>
       <Link to="/Ingredients" ><img src={image} alt="" className={'rounded-lg brightness-50 contrast-125 h-[16rem] duration-300 ease-linear'}/></Link>
       <h1 className='absolute text-3xl font-medium drop-shadow-lg cursor-pointer'>{text}</h1>
     </div>
