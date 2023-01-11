4c9804f90c7f9a47dcc2b23e3eba28e0348945f7
Madalina Dumitrascu <89780920+MadalinaDumitrascu@users.noreply.github.com>
2023-01-11 13:40:30 +0200

    added the About page

--- React-app-Recipo/react-app/src/App.js
+++ React-app-Recipo/react-app/src/App.js
@@ -7,20 +7,21 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Subscriptions from "./Pages/Subscriptions";
 import Register from "./Pages/Register";
-
+import About from "./Pages/About";
 
 function App() {
-  
+
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path='/Login' element={<Login/>} />
-        <Route path="/Register" element={<Register/>} />
-        <Route path='/Dashboard' element={<Dashboard/>} />
-        <Route path="/Ingredients" element={<Ingredients/>} />
-        <Route path='/Subscriptions' element={<Subscriptions />}/>
+        <Route path="/" element={<Home />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path="/Ingredients" element={<Ingredients />} />
+        <Route path='/Subscriptions' element={<Subscriptions />} />
+        <Route path="/About" element={<About />} />
       </Routes>
       <Footer />
     </div>
---
+++ React-app-Recipo/react-app/src/Pages/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Parallax } from 'react-parallax'
+import BackgroundImage from '../Images/empty_fridge.jpg'
+
+const About = () => {
+    return (
+        <div className=''>
+            <Parallax bgImage={BackgroundImage} className='w-full h-screen flex items-center relative bg-black' renderLayer={(percentage) => (
+                <div
+                    style={{
+                        position: 'absolute',
+                        width: '100%',
+                        height: '100%',
+                        background: `rgba(0, 0, 0, ${percentage * 0.5})`,
+                    }}
+                />
+            )} strength={0} blur={5}>
+                <div className='bg-orange-200 drop-shadow-lg p-20 rounded-xl m-60'>
+                    <h1 className='text-3xl font-semibold'>About Us</h1>
+                    <br/>
+                    <p className='text-xl '>We, at Agilis, started working together as a team a year ago, and ever since we tried to create unique apps which will make our customer's life easier.</p>
+                    <br/>
+                    <br />
+                    <h1 className='text-3xl font-semibold'>About Recipo</h1>
+                    <br />
+                    <p className='text-xl'>Ever felt like you ran out of ideas when it comes to cooking the next meal?
+                        Well, it happened to us a lot and that's how we came up with RECIPO.
+                    This app is intended to be a Cooking book just one click away, but based on what ingredients you have available.</p>
+
+
+                </div>
+            </Parallax>
+        </div>
+    )
+}
+
+export default About
