3b0aed03285a9f7bb2326443073107457c3f2343
Madalina Dumitrascu <89780920+MadalinaDumitrascu@users.noreply.github.com>
2023-01-19 21:09:27 +0200

    added the RecipeByUser Model and Controller

---
+++ Recipo-by-Agilis/Recipo-by-Agilis/Controllers/RecipeByUserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Recipo_by_Agilis.Models;
+
+namespace Recipo_by_Agilis.Controllers;
+
+
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeByUserController : ControllerBase
+    {
+        private readonly RecipoContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RecipeByUserController(RecipoContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+    }
---
+++ Recipo-by-Agilis/Recipo-by-Agilis/Models/RecipeByUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Recipo_by_Agilis.Models;
+
+public class RecipeByUser
+{
+    public int Id { get; set; }
+    public int RecipeId { get; set; }
+    public string UserId { get; set; }
+}
